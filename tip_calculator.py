15975ed582a2c8e11d12f7714dbc70803a966a9e
MylesPerHour201 <beck.mj1993@gmail.com>
2023-09-25 21:18:22 -0400

    passed unittest

--- tip_calculator.py
+++ tip_calculator.py
@@ -1,7 +1,19 @@
 # tip_calculator.py
+# AS A GROUP, POD 1, WE COLLECTIVELY WORKED ON THIS ASSIGNMENT UNTIL 9:15PM 9/25/23 AND PASSED UNITTEST
 
 # TODO: Create a function named calculate_tip
-    try:  #DO NOT MODIFY
+try:  #DO NOT MODIFY
+        def tip_calculator():
+            total_cost = float(input('The cost of the meal (without tax)'))
+            num_people = int(input('The number of people splitting the bill'))
+            tip_percentage = float(input('The tip percentage'))
+            sales_tax = total_cost * .10
+            tip = (total_cost * tip_percentage) / 100
+            total_bill = float(total_cost + sales_tax + tip)
+            bill_per_person = float(total_bill / num_people)
+
+            print(f'Total bill: ${total_bill:.2f}')
+            print(f'Each person should pay: ${bill_per_person:.2f}')
 
 
     # TODO:
@@ -28,9 +40,11 @@
 
     # NOTE: The amounts are displayed with 2 decimals only
 
-    except: # TODO: modify this except to include a Built-in Exception
+except: # TODO: modify this except to include a Built-in Exception
+        total_cost = str
+
         # TODO: Print an statement telling the user their input is invalid 
-    
+        print('Your input is invalid')
     
 if __name__ == "__main__": # DO NOT MODIFY
     tip_calculator() # DO NOT MODIFY
